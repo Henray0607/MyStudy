5c3669848ce23fdcbfc807501dc592af01c9bd16
Henray0607 <1084632143@qq.com>
2023-09-16 00:57:14 +0800

    455. 分发饼干

--- Mystudy/src/main/java/greedy/Solution455.java
+++ Mystudy/src/main/java/greedy/Solution455.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 /**
  * 455. 分发饼干
  */
-public class Solution452 {
+public class Solution455 {
     public static int findContentChildren(int[] g, int[] s) {
         if (g == null || g.length == 0 || s == null || s.length == 0) {
             return 0;
