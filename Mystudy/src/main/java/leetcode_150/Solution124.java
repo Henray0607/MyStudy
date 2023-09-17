c37ff7163053da2eaf36a73ca297ca4fb2366ada
Henray0607 <1084632143@qq.com>
2023-09-17 23:37:15 +0800

    124. 二叉树中的最大路径和

---
+++ Mystudy/src/main/java/leetcode_150/Solution124.java
@@ -0,0 +1,25 @@
+package leetcode_150;
+
+import model.TreeNode;
+
+public class Solution124 {
+    int count = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        traverse(root);
+        return count;
+    }
+
+    public int traverse(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftMax = Math.max(0, traverse(node.left));
+        int rightMax = Math.max(0, traverse(node.right));
+        count = Math.max(count, leftMax + rightMax + node.val);
+        return Math.max(leftMax, rightMax) + node.val;
+    }
+}
