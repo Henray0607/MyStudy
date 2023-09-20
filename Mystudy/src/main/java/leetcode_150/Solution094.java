964ed1c7372febfc929d84e090392abd4aa0ad9c
Henray0607 <1084632143@qq.com>
2023-09-20 22:51:22 +0800

    94. 二叉树的中序遍历

---
+++ Mystudy/src/main/java/leetcode_150/Solution094.java
@@ -0,0 +1,19 @@
+package leetcode_150;
+
+import model.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution094 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        list.addAll(inorderTraversal(root.left));
+        list.add(root.val);
+        list.addAll(inorderTraversal(root.right));
+        return list;
+    }
+}
