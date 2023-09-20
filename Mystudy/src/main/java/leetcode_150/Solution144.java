37da8bd2b94613ccaf0dad2275948892057ff838
Henray0607 <1084632143@qq.com>
2023-09-20 22:52:22 +0800

    144. 二叉树的前序遍历

---
+++ Mystudy/src/main/java/leetcode_150/Solution144.java
@@ -0,0 +1,19 @@
+package leetcode_150;
+
+import model.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution144 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        list.add(root.val);
+        list.addAll(preorderTraversal(root.left));
+        list.addAll(preorderTraversal(root.right));
+        return list;
+    }
+}
