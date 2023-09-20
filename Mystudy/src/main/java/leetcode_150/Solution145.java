7beefd90a27b8d1531b5753d1e9f036abfe87ffe
Henray0607 <1084632143@qq.com>
2023-09-20 22:52:36 +0800

    145. 二叉树的后序遍历

---
+++ Mystudy/src/main/java/leetcode_150/Solution145.java
@@ -0,0 +1,19 @@
+package leetcode_150;
+
+import model.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution145 {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        list.addAll(postorderTraversal(root.left));
+        list.addAll(postorderTraversal(root.right));
+        list.add(root.val);
+        return list;
+    }
+}
