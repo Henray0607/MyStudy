9b559543b1675d9224d2ad2f847dc60f1fddb429
Henray0607 <1084632143@qq.com>
2023-10-06 23:10:20 +0800

    121. 买卖股票的最佳时机

--- Mystudy/src/main/java/leetcode_150/Solution121.java
+++ Mystudy/src/main/java/leetcode_150/Solution121.java
@@ -19,8 +19,41 @@ public class Solution121 {
         return maxProfit;
     }
 
+    public static int maxProfit2(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int n = prices.length;
+        int[][] dp = new int[n + 1][n + 1];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+
+    public static int maxProfit3(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int n = prices.length;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = -prices[0];
+        for (int i = 1; i < n; i++) {
+            dp[0] = Math.max(dp[0], dp[1] + prices[i]);
+            dp[1] = Math.max(dp[1], -prices[i]);
+        }
+        return dp[0];
+    }
+
     public static void main(String[] args) {
         int[] prices = {7, 1, 5, 3, 6, 4};
         System.out.println(maxProfit(prices));
+
+        int[] prices2 = {1};
+        System.out.println(maxProfit2(prices2));
     }
 }
