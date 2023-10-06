d4a2afb409aa4e3a0b8ee1ba90539c7aff560760
Henray0607 <1084632143@qq.com>
2023-10-06 23:24:12 +0800

    121. 买卖股票的最佳时机

--- Mystudy/src/main/java/leetcode_150/Solution121.java
+++ Mystudy/src/main/java/leetcode_150/Solution121.java
@@ -2,6 +2,14 @@ package leetcode_150;
 
 /**
  * 121.买卖股票的最佳时机
+ *
+ * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+ * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+ * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+ *
+ * 分析：假设我们想在第 i 天把股票卖出，那么此时能得到的最大收益 maxProfit[i] 一定是 price[i] 减去 第 i 天之前的最小价格 minPrice。
+ * 因此我们可以使用一个变量 minPrice 来维护截至第 i 天的最小价格，一个变量 maxProfit 维护截至第 i 天得到的最大收益。
+ * 我们只要取选择出所有 maxProfit[i] 中的最大值，即可得到结果。
  */
 public class Solution121 {
     public static int maxProfit(int[] prices) {
