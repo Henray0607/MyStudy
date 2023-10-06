be2c5cb93239730043b8a5a636cfc95b4e9a03a7
Henray0607 <1084632143@qq.com>
2023-10-06 23:29:53 +0800

    122.买卖股票的最佳时机 II

--- Mystudy/src/main/java/leetcode_150/Solution122.java
+++ Mystudy/src/main/java/leetcode_150/Solution122.java
@@ -1,5 +1,12 @@
 package leetcode_150;
 
+/**
+ * 122.买卖股票的最佳时机 II（交易次数无限制 k = 无穷）
+ *
+ * 给你一个整数数组 prices ，其中 prices[i] 表示某支股票第 i 天的价格。
+ * 在每一天，你可以决定是否购买和/或出售股票。你在任何时候 最多 只能持有 一股 股票。你也可以先购买，然后在 同一天 出售。
+ * 返回 你能获得的 最大 利润 。
+ */
 public class Solution122 {
     public static int maxProfit(int[] prices) {
         if (prices == null || prices.length == 0) {
@@ -10,6 +17,36 @@ public class Solution122 {
             maxProfit += Math.max(prices[i] - prices[i - 1], 0);
         }
         return maxProfit;
+    }
+
+    public static int maxProfit2(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int n = prices.length;
+        int[][] dp = new int[n + 1][n + 1];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+
+    public static int maxProfit3(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int n = prices.length;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = -prices[0];
+        for (int i = 1; i < n; i++) {
+            dp[0] = Math.max(dp[0], dp[1] + prices[i]);
+            dp[1] = Math.max(dp[1], dp[0] - prices[i]);
+        }
+        return dp[0];
     }
 
     public static void main(String[] args) {
