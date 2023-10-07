760a54636c355a336b3953606c9cb3ae110a99f8
Henray0607 <1084632143@qq.com>
2023-10-07 23:45:37 +0800

    LCR 188.买卖芯片的最佳时机

---
+++ Mystudy/src/main/java/lcr/Solution188.java
@@ -0,0 +1,19 @@
+package lcr;
+
+/**
+ * LCR 188.买卖芯片的最佳时机
+ */
+public class Solution188 {
+    public int bestTiming(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int maxProfit = 0;
+        int minPrice = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            minPrice = Math.min(minPrice, prices[i]);
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+        }
+        return maxProfit;
+    }
+}
