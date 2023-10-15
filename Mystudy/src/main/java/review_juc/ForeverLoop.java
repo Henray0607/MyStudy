807550a03d69d18b0a62b79d636369826dc6331c
Henray0607 <1084632143@qq.com>
2023-10-15 10:31:39 +0800

    volatile-保证线程间的可见性

---
+++ Mystudy/src/main/java/review_juc/ForeverLoop.java
@@ -0,0 +1,40 @@
+package review_juc;
+
+/**
+ * 可见性案例
+ */
+public class ForeverLoop {
+    // public static boolean stop = false;
+    // 对多个线程的可见性
+    public static volatile boolean stop = false;
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            stop = true;
+            System.out.println(Thread.currentThread().getName() + ": modify stop to true...");
+        }, "t1").start();
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + ": " + stop);
+        }, "t2").start();
+
+        // VM Options配置参数:-Xint 禁用即时编译器
+        new Thread(() -> {
+            int i = 0;
+            while (!stop) {
+                i++;
+            }
+            System.out.println("stopped... count: " + i);
+        }, "t3").start();
+    }
+}
