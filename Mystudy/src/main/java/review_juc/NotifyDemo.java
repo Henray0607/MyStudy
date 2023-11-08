93456dd4acfc9b062112c0e87945e37030d2a42c
Henray0607 <1084632143@qq.com>
2023-11-08 22:07:09 +0800

    notify()和notifyAll()

--- Mystudy/src/main/java/review_juc/NotifyDemo.java
+++ Mystudy/src/main/java/review_juc/NotifyDemo.java
@@ -6,9 +6,12 @@ public class NotifyDemo {
 
     public static void main(String[] args) throws InterruptedException {
         Thread t1 = new Thread(() -> {
+            // 加锁同步块，使用 lock 对象作为锁。只有一个线程可以进入这个同步块，其他线程必须等待直到锁被释放。
             synchronized (lock) {
                 System.out.println(Thread.currentThread().getName() + "...waiting...");
                 try {
+                    // 等待操作，它让当前线程进入等待状态，并释放 lock 锁。
+                    // 线程将一直等待，直到另一个线程调用相同lock对象的notify()或notifyAll()方法，唤醒它。
                     lock.wait();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
